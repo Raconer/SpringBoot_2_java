515f110f01ac0aa9ce25d16a68a8ee4eef33998d
DongHoKim <cha11enge@hanmail.net>
2023-07-31 23:23:06 +0900

    first commit

---
+++ src/main/java/com/spring/java/SpringBoot2JApplication.java
@@ -0,0 +1,13 @@
+package com.spring.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2JApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2JApplication.class, args);
+    }
+
+}
